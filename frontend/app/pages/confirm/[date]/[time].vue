c6e2e94115f35ff4bb7de682c2d87ead0cf698a1
hirata kyouko <tonkinese0chantilly.2615@gmail.com>
2026-07-23 16:50:05 +0900

    feat：予約確認画面のUI作成

--- frontend/app/pages/confirm/[date]/[time].vue
+++ frontend/app/pages/confirm/[date]/[time].vue
@@ -1,8 +1,37 @@
 <template>
-    <p>確認画面</p>
-    <p>予約者は{{ user.name }}さん</p>
-    <p>メールアドレスは{{ user.email }}</p>
-    <div>予約日時は{{ date }}の{{ time }}</div>
+    <div class="confirm">
+        <div class="confirm-content">
+            <h2 class="title">予約内容の確認</h2>
+            <form class="confirm-form">
+                <p class="section-title">【ご予約者情報】</p>
+                <div class="group">
+                    <div class="item-group">
+                        <p class="label">名前</p>
+                        <p class="item">{{ user.name }}</p>
+                    </div>
+                    <div class="item-group">
+                        <p class="label">メールアドレス</p>
+                        <p class="item">{{ user.email }}</p>
+                    </div>
+                </div>
+                <p class="section-title">【予約内容】</p>
+                <div class="group">
+                    <div class="item-group">
+                        <p class="label">予約日</p>
+                        <p class="item">{{ displayDate }}</p>
+                    </div>
+                    <div class="item-group">
+                        <p class="label">予約時間</p>
+                        <p class="item">{{ time }}</p>
+                    </div>
+                </div>
+                <div class="btn-area">
+                    <button class="reservation-btn">予約する</button>
+                    <button class="return-btn">予約一覧へ戻る</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </template>
 
 <script setup>
@@ -11,6 +40,11 @@ const route = useRoute();
 // 予約日時取得
 const date = route.params.date;
 const time = route.params.time;
+// 年月日表示用
+const year = date.substring(0, 4);
+const month = date.substring(5, 7);
+const dates = date.substring(8, 10);
+const displayDate = `${year}年${month}月${dates}日`;
 // ユーザー情報取得
 const user = ref("");
 // トークン取得
@@ -26,3 +60,101 @@ onMounted(async () => {
     user.value = userRes;
 });
 </script>
+
+<style scoped>
+p {
+    margin: 0;
+}
+
+.confirm {
+    background-color: #cce9fa;
+    width: 100%;
+    height: 90vh;
+    text-align: center;
+}
+
+.confirm-content {
+    padding-top: 80px;
+}
+
+.title {
+    margin: 0;
+    font-size: 40px;
+    color: #304654;
+}
+
+.confirm-form {
+    width: 50%;
+    max-width: 600px;
+    margin: 80px auto;
+    padding: 30px 50px;
+    border: 1px solid #304654;
+    border-radius: 20px;
+    background-color: #eef9ff;
+}
+
+.section-title {
+    font-size: 20px;
+    font-weight: bold;
+    color: #304654;
+    text-align: left;
+    margin-top: 20px;
+}
+
+.group {
+    background-color: #ffffff;
+    padding: 15px;
+    border-radius: 10px;
+}
+
+.item-group {
+    margin-bottom: 30px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+}
+
+.label {
+    font-size: 20px;
+    font-weight: bold;
+    color: #304654;
+    text-align: left;
+}
+
+.item {
+    font-size: 18px;
+    color: #304654;
+    text-align: left;
+}
+
+.btn-area {
+    display: flex;
+    justify-content: center;
+    margin-top: 30px;
+    margin-bottom: 20px;
+    height: 50px;
+}
+
+.reservation-btn {
+    border: none;
+    background-color: #da251d;
+    color: #eef9ff;
+    padding: 10px 20px;
+    margin: 0 20px 0;
+    font-size: 20px;
+    cursor: pointer;
+    width: 35%;
+}
+
+.return-btn {
+    border: none;
+    background-color: #666666;
+    opacity: 0.5;
+    color: #eef9ff;
+    padding: 10px 20px;
+    margin: 0 20px 0;
+    font-size: 20px;
+    cursor: pointer;
+    width: 35%;
+}
+</style>
