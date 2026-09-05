8253c329d74a92382e51accadc7d20d0affd0a92
hirata kyouko <tonkinese0chantilly.2615@gmail.com>
2026-09-05 23:37:07 +0900

    feat：プロフィール設定画面 vee-validateとyupを用いたバリデーション機能の追加
    バリデーション表示中は「登録する」ボタンを無効化

--- frontend/app/pages/profile.vue
+++ frontend/app/pages/profile.vue
@@ -20,6 +20,8 @@
                         v-model="address"
                         placeholder="例：東京都新宿区西新宿2-8-1"
                     />
+                    <!-- vee-validateのバリデーション -->
+                    <p class="error">{{ errors.address }}</p>
                 </div>
                 <div class="group">
                     <p class="item">電話番号</p>
@@ -29,8 +31,20 @@
                         v-model="phone"
                         placeholder="例：0901112222"
                     />
+                    <!-- vee-validateのバリデーション -->
+                    <p class="error">{{ errors.phone }}</p>
                 </div>
-                <button class="profile-btn" type="submit">登録する</button>
+                <!-- バリデーションの表示中はclass変更 & クリック不可 -->
+                <button
+                    class="profile-btn"
+                    type="submit"
+                    v-bind:class="{
+                        'is-disabled-btn': btnIsInvalid,
+                    }"
+                    :disabled="btnIsInvalid"
+                >
+                    登録する
+                </button>
                 <div class="profile-link">
                     <NuxtLink class="" to="/list">設定せず一覧画面へ</NuxtLink>
                 </div>
@@ -40,14 +54,43 @@
 </template>
 
 <script setup>
+// インポート
+import { useForm, useField } from "vee-validate";
+import * as yup from "yup";
+import { ref, computed } from "vue";
+
+// バリデーションのルールを設定
+const schema = yup.object({
+    address: yup.string().nullable().max(255, "255文字以下で入力してください"),
+    phone: yup
+        .string()
+        .nullable()
+        // 空文字の場合はバリデーションを免除する設定
+        .transform((value) => (value === "" ? null : value))
+        .matches(
+            /^0\d{9,10}$/,
+            "正しい電話番号（10桁または11桁）を入力してください",
+        ),
+});
+// クライアントエラーを格納するオブジェクト
+const { errors } = useForm({
+    validationSchema: schema,
+});
+// エラーを格納するオブジェクト
+const { value: address } = useField("address");
+const { value: phone } = useField("phone");
+
 // プロフィール入力値
 const gender = ref("");
-const address = ref("");
-const phone = ref("");
 
 // 認証中のみアクセス可能にする
 definePageMeta({
     middleware: "auth",
+});
+
+// バリデーション表示の有無によって、ボタンのclassとdisabledを変更する
+const btnIsInvalid = computed(() => {
+    return Object.keys(errors.value).length > 0;
 });
 
 // プロフィール設定
@@ -127,6 +170,11 @@ p {
     padding: 10px;
 }
 
+.error {
+    color: #da251d;
+    text-align: left;
+}
+
 .profile-btn {
     border: none;
     background-color: #da251d;
@@ -136,6 +184,12 @@ p {
     cursor: pointer;
     width: 70%;
     margin-bottom: 20px;
+}
+
+.is-disabled-btn {
+    background-color: #666666;
+    opacity: 0.5;
+    cursor: auto;
 }
 
 .profile-link {
