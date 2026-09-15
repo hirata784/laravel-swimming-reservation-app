6a88bec1b5d59039b242be15d8e395baf46f10db
hirata kyouko <tonkinese0chantilly.2615@gmail.com>
2026-09-15 22:23:25 +0900

    refactor：TimeSlotControllerの取得データを整理
    reserved_countとis_availableの取得処理を削除
    
    TimeSlotのみを取得するように変更

--- backend/src/app/Http/Controllers/TimeSlotController.php
+++ backend/src/app/Http/Controllers/TimeSlotController.php
@@ -13,20 +13,8 @@ class TimeSlotController extends Controller
      */
     public function index()
     {
-        // time_slotsテーブルとreservationsテーブルの情報を一緒に取得(予約数がreservations_countとして追加される)
-        $items = TimeSlot::withCount('reservations')->get()
-            // フロント用にレスポンス形式を整形
-            ->map(function ($item) {
-                return [
-                    'id' => $item->id,
-                    'date' => $item->date,
-                    'start_time' => $item->start_time,
-                    'capacity' => $item->capacity,
-                    'reserved_count' => $item->reservations_count,  // 予約人数（reservationsテーブルの件数）
-                    // 予約人数が定員未満なら予約可能（true）、満席ならfalse
-                    'is_available' => $item->reservations_count < $item->capacity
-                ];
-            });
+        // 全データ取得
+        $items = TimeSlot::all();
 
         return response()->json([
             'data' => $items
