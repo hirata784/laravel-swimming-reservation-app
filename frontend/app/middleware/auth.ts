43ca2b8ae1aa274d6328f80a29f872bd6198605e
hirata kyouko <tonkinese0chantilly.2615@gmail.com>
2026-09-17 20:38:30 +0900

    feat：未認証時に予約一覧画面から日時と操作モードを引き継ぐ機能を追加
    予約一覧画面で選択した日時をログイン画面へ引き継ぐよう変更
    
    予約確認画面の操作モードをログイン画面へ引き継ぐよう変更

--- frontend/app/middleware/auth.ts
+++ frontend/app/middleware/auth.ts
@@ -1,9 +1,16 @@
-export default defineNuxtRouteMiddleware(() => {
+export default defineNuxtRouteMiddleware((to) => {
     // トークンを取得
     const token = useCookie("token");
     // 認証中のみ画面遷移できる
     // 未認証中に遷移すると、ログイン画面へ遷移する
     if (!token.value) {
-        return navigateTo("/login");
+        return navigateTo({
+            path: "/login",
+            // 日時を含んだパスが入る
+            query: {
+                redirect: to.path,
+                mode: to.query.mode,
+            },
+        });
     }
 });
