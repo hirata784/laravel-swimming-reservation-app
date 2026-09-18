e0256634fab3858cd9ef6343ff82e8d982709a18
hirata kyouko <tonkinese0chantilly.2615@gmail.com>
2026-09-18 12:13:11 +0900

    feat：予約枠(time_slotsテーブル)の不足データを自動作成
    予約一覧表示時に今日から7日分のtime_slotsテーブルデータを確認
    
    不足している日時の予約枠を自動作成

--- backend/src/app/Http/Controllers/TimeSlotController.php
+++ backend/src/app/Http/Controllers/TimeSlotController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\TimeSlot;
+use Illuminate\Support\Carbon;
 
 class TimeSlotController extends Controller
 {
@@ -13,6 +14,29 @@ class TimeSlotController extends Controller
      */
     public function index()
     {
+        // 予約一覧に表示してる日付がtime_slotsテーブルに存在していない場合、データ追加
+        for ($i = 0; $i < 7; $i++) {
+            // 日付を取得
+            $targetDate = Carbon::today()->addDays($i);
+            // 日付が存在しているかチェック
+            $exist = TimeSlot::whereDate('date',  $targetDate)->exists();
+            // もし存在していなければデータ追加
+            if (!$exist) {
+                for ($j = 9; $j <= 18; $j++) {
+                    TimeSlot::create([
+                        'date' => $targetDate->toDateString(),
+                        'start_time' => $j . ':00:00',
+                        'capacity' => 7,
+                    ]);
+                    TimeSlot::create([
+                        'date' => $targetDate->toDateString(),
+                        'start_time' => $j . ':30:00',
+                        'capacity' => 7,
+                    ]);
+                }
+            }
+        }
+
         // 全データ取得
         $items = TimeSlot::all();
 
