b96c53b3128394c5575b3477ed684c3e370586fc
hirata kyouko <tonkinese0chantilly.2615@gmail.com>
2026-09-20 16:04:14 +0900

    refactor：ReservationControllerの不要な予約枠自動生成処理を削除
    理由：
    
    予約一覧画面の表示時に、対象日の予約枠データが存在しない場合のみ自動生成（補完）する機能を追加したため
    
    これにより、予約保存時（storeメソッド）における未存在日時の追加処理が不要となったため

--- backend/src/app/Http/Controllers/ReservationController.php
+++ backend/src/app/Http/Controllers/ReservationController.php
@@ -43,18 +43,6 @@ class ReservationController extends Controller
         // 予約枠idを検索してから取得
         $time_slot = TimeSlot::where('date', $request->date)->where('start_time', $request->start_time)->first();
 
-        // 予約枠テーブルに存在しない日時の場合、新たにデータを追加する
-        if ($time_slot === null) {
-            TimeSlot::create(
-                [
-                    'date' => $request->date,
-                    'start_time' => $request->start_time,
-                ]
-            );
-            // データ追加後、改めて予約枠idを検索してから取得
-            $time_slot = TimeSlot::where('date', $request->date)->where('start_time', $request->start_time)->first();
-        }
-
         $time_slot_id = $time_slot->id;
         // 予約データを作成
         $item = Reservation::create(
